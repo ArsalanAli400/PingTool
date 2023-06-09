80514a63edf32ac2d117547d59d0a01be48372db
ArsalanAli400 <arsalan.ali.safeer@gmail.com>
2023-06-09 16:59:11 +0500

    Updated main file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "nettools.hpp"
 
+void execute_application(nettools net, int _argc_, char *_argv_[]){
+    std::cout << "Connecting with " << _argv_[1] << std::endl;
+    float percentage = net.ping10(_argc_, _argv_);
+    std::cout << "connection pingrate: " << percentage << std::endl;
+    // Exceteable application
+    if (percentage > 50) {
+        int i;
+        std::cout << "Checking if processor is available..." << std::endl;
+        if (system(NULL)) puts("Ok");
+        else exit(1);
+        std::cout << "Executing Calc... " << std::endl;
+        i = system(
+                "/home/arsalan/Nextcloud/Arsalan/development/cNet_ping/cmake-build-debug/ping 192.168.10.1");   // For local services
+        std::cout << "The value returned was: " << i << std::endl;
+        getchar();
+    }else{
+        std::cout << "No network access! can't start application" << std::endl;
+    }
+}
+
 int main(int argc,char *argv[]) {
     nettools nt;                // Net tool calling
-    std::cout << "Connecting with " << argv[1] << std::endl;
-    float percentage = nt.ping10(argc, argv);
-    std::cout << "connection pingrate: " << percentage << std::endl;
+    execute_application(nt,argc,argv);
     return 0;
 }
